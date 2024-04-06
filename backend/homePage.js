010a164733f396063ff3f96f189bd5aeed7a6e49
Iris Veress <irisveress@gmail.com>
2024-04-06 08:55:56 +0300

    home page frontend

--- backend/homePage.js
+++ backend/homePage.js
@@ -4,43 +4,41 @@ import { addAppToDatabase } from "./firebase.js";
 const appNameText = document.getElementById("app-name-text");
 const submitApp = document.getElementById("submit-app");
 
-submitApp.addEventListener("click", () => {
-  const appName = appNameText.value;
-  addAppToDatabase(appName);
-  publicDashboardEl.innerHTML = "";
-  renderApps();
-});
-
 // FRONTEND JS
 
 // HTML elements
 const logButtonEl = document.getElementById("login-and-logout");
 const publicViewButtonEl = document.getElementById("view-public-dashboard");
 const devViewButtonEl = document.getElementById("view-developer-dashboard");
-const publicDashboardEl = document.getElementById("public_view");
+const dashboardEl = document.getElementById("public_view");
 const devDashboardNoLoginEl = document.getElementById("dev_view-no_login");
 const devDashboardEl = document.getElementById("dev_view");
-
-// Values from HTML elements
-const logStatus = logButtonEl.textContent;
 
 //Click handlers
 const publicBtnClickHandler = () => {
   // console.log('buburubu')
-  publicDashboardEl.style.display = "grid";
-  devDashboardNoLoginEl.classList.add("hidden");
-  devDashboardEl.classList.add("hidden");
+  document.getElementById("submit-new").remove();
+  renderApps(`/apps`);
+  dashboardEl.style.display = "grid";
 };
 const devBtnClickHandler = () => {
-  // console.log('mimimimimimimi mimimimi only me :D')
-  publicDashboardEl.style.display = "none";
-  if (logStatus === "Log in") {
-    // console.log('mimimo');
-    devDashboardNoLoginEl.classList.remove("hidden");
-    devDashboardEl.style.display = "none";
-  } else if (logStatus === "Log out") {
-    devDashboardNoLoginEl.classList.add("hidden");
-    devDashboardEl.style.display = "grid";
+  if (logButtonEl.textContent === "Login") {
+    dashboardEl.innerHTML = "You must be logged in";
+  } else if (logButtonEl.textContent === "Logout") {
+    renderApps(`users/${localStorage.getItem("currentUser")}/apps`);
+    const newAppHTML = `<div id="submit-new">
+      <h2>Submit a new app:</h2>
+      <input id="app-name-text" type="text" />
+      <button id="submit-app"><p>Add app</p></button>
+    </div>`;
+    if (document.getElementById("submit-new") !== null) return;
+    dashboardEl.insertAdjacentHTML("afterend", newAppHTML);
+    submitApp.addEventListener("click", () => {
+      const appName = appNameText.value;
+      addAppToDatabase(appName);
+      publicDashboardEl.innerHTML = "";
+      renderApps();
+    });
   } else {
     console.log(error);
   }
@@ -54,23 +52,9 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
 //functions
 
-// function renderApps() {
-//   get(child(ref(database), "apps/"))
-//     .then((snapshot) => {
-//       if (snapshot.exists()) {
-//         snapshot.forEach((childSnapshot) => {
-//           renderNewApp(childSnapshot.key, childSnapshot.val().status, 0);
-//         });
-//       } else {
-//         console.log("No data available");
-//       }
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// }
-function renderApps() {
-  get(child(ref(database), `users/${localStorage.getItem("currentUser")}/apps`))
+function renderApps(path) {
+  dashboardEl.innerHTML = "";
+  get(child(ref(database), path))
     .then((snapshot) => {
       if (snapshot.exists()) {
         snapshot.forEach((childSnapshot) => {
@@ -95,11 +79,10 @@ const renderNewApp = (appName, appStatus, nrOfEndpoints) => {
       <p class="section">Endpoints: ${nrOfEndpoints}</p>
       <a href="../html/endpoints.html">See more</a>
     </div>`;
-  publicDashboardEl.insertAdjacentHTML("beforeend", newAppHTML);
+  dashboardEl.insertAdjacentHTML("beforeend", newAppHTML);
 };
 
-console.log(logStatus);
 publicViewButtonEl.addEventListener("click", publicBtnClickHandler);
 devViewButtonEl.addEventListener("click", devBtnClickHandler);
 
-renderApps();
+renderApps(`/apps`);

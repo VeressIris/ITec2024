54655f5f28fca16d0ee05989aa7e725483014bb4
m-ag-da <146845795+m-ag-da@users.noreply.github.com>
2024-04-06 10:39:47 +0300

    dying

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -62,16 +62,57 @@ function renderNewEndpoint(endPName, endPStatus) {
   dashboard.appendChild(bugReports);
   document.getElementById("endpoints_box").appendChild(dashboard);
 }
-//Not working
 function renderNewDevEndpoint(endPName, endPStatus) {
-  getBugList();
-  let bugListDb;
-  let bugListEl = document.getElementById("bugList");
-  for (i = 0; i < bugListDb.length; ++i) {
-    let liAux = document.createElement("li");
-    liAux.innerHTML = `<li><label><input type="checkbox">${bugListDb[i]}</label></li>`;
-    bugListEl.appendChild(li);
-  }
+  const dashboard = document.createElement("div");
+  dashboard.className = "dashboard";
+  const endPointName = document.createElement("h2");
+  endPointName.innerText = endPName;
+  dashboard.appendChild(endPointName);
+  const status = document.createElement("p");
+  status.className = "section";
+  status.innerText = `Status: ${endPStatus}`;
+  dashboard.appendChild(status);
+  const history = document.createElement("p");
+  history.className = "section";
+  history.innerText = "History:";
+  dashboard.appendChild(history);
+  const bugList = document.createElement("div");
+  bugList.id = "bug-list";
+  
+  // const bugReportTitle = document.createElement("p");
+  // bugReportTitle.className = "section";
+  // bugReportTitle.innerText = "Bug reports:";
+  // bugReports.appendChild(bugReportTitle);
+  // const bugText = document.createElement("input");
+  // bugText.type = "text";
+  // bugText.className = "bug-text";
+  // bugReports.appendChild(bugText);
+  // const submitButton = document.createElement("button");
+  // submitButton.className = "submit-bug";
+  // submitButton.innerHTML = "<p>Submit</p>";
+  // submitButton.addEventListener("click", () => {
+  //   submitBug(bugText.value, appName.innerHTML, endPName);
+  // });
+  // bugReports.appendChild(submitButton);
+  // dashboard.appendChild(bugReports);
+  // document.getElementById("endpoints_box").appendChild(dashboard);
+}
+//Not working
+function renderDevEndpoints(endPName, endPStatus) {
+  get(child(ref(database), `apps/${appName.innerHTML}/endpoints`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          console.log(childSnapshot);
+          renderNewEndpoint(childSnapshot.key, childSnapshot.val().status);
+        });
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 endpointSubmit.addEventListener("click", () => {

c7774aded67c7b426b7635460c40316b175914e5
Iris Veress <irisveress@gmail.com>
2024-04-06 03:38:13 +0300

    add bug

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -2,11 +2,15 @@ import { submitBug } from "./firebase.js";
 
 const appName = document.getElementById("app-name");
 const endpoints = document.getElementsByClassName("endpoint-name");
-const endpointBugSubmit = document.getElementByClassName("submit-bug");
-const endpointBugText = document.getElementByClassName("bug-text");
+const endpointBugSubmit = document.getElementsByClassName("submit-bug");
+const endpointBugText = document.getElementsByClassName("bug-text");
 
 for (let i = 0; i < endpoints.length; i++) {
   endpointBugSubmit[i].addEventListener("click", () => {
-    submitBug(endpointBugText.value, appName.innerHTML, endpoints[i].innerText);
+    submitBug(
+      endpointBugText[i].value,
+      appName.innerHTML,
+      endpoints[i].innerText
+    );
   });
 }
